ff60f0d163a5143a5596af28ee0affe13c016d55
WH1909030 <Scott_Niu@wistronits.com>
2020-08-25 16:15:01 +0800

    演示了如何在 Spring Boot 中通过 actuator 检查项目运行情况

---
+++ spring-boot-all-actuator/src/main/java/com/ntm/actuator/SpringBootAllActuatorApplication.java
@@ -0,0 +1,18 @@
+package com.ntm.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @description: 启动类
+ * @author: Tian
+ * @date: 2020-08-25 15:55:37
+ * @version: V1.0
+ */
+@SpringBootApplication
+public class SpringBootAllActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAllActuatorApplication.class, args);
+	}
+
+}

da7e0df8ba697a91f8ef40e98530b945871506d7
WH1909030 <Scott_Niu@wistronits.com>
2020-09-03 15:19:01 +0800

    测试logback记录日志

---
+++ spring-boot-all-logback/src/test/java/com/ntm/logback/SpringBootAllLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntm.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAllLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
